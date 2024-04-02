d1069cdf9db03e78cdb2b00fe32b20c6d2ab8f31
Caique Queiroz <caiqueq34@outlook.com>
2024-04-02 00:30:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,168 @@
+function es(){
+    var vuf= document.getElementById("uf").value;
+    switch(vuf){
+        case "AC":
+            {
+                document.getElementById("capital").value= "RIO BRANCO";
+                document.getElementById("nascidos").value= "ACREANO";
+                document.getElementById("capnascidos").value= "RIO-BRANQUENSES";
+                break;
+            }
+            case 'AL':{
+                document.getElementById("capital").value="MACEIÓ";
+                document.getElementById("nascidos").value="ALAGOANOS";
+                document.getElementById("capnascidos").value="MACEIOENSES";
+                break;
+               }
+               case 'AM':{
+                document.getElementById("capital").value="MANAUS";
+                document.getElementById("nascidos").value="AMAZONENSES";
+                document.getElementById("capnascidos").value="MANAUARAS";
+                break;
+               }
+               case 'AP':{
+                document.getElementById("capital").value="MACAPÁ";
+                document.getElementById("nascidos").value="AMAPAENSES";
+                document.getElementById("capnascidos").value="MACAPAENSES";
+                break;
+               }
+               case 'BA':{
+                document.getElementById("capital").value="SALVADOR";
+                document.getElementById("nascidos").value="BAIANOS";
+                document.getElementById("capnascidos").value="SOTEROPOLITANOS";
+                break;
+               }
+               case 'CE':{
+                document.getElementById("capital").value="FORTALEZA";
+                document.getElementById("nascidos").value="CEARENSES";
+                document.getElementById("capnascidos").value="FORTALEZENSES";
+                break;
+               }
+               case 'DF':{
+                document.getElementById("capital").value="BRASILIA";
+                document.getElementById("nascidos").value="BRASILIENSES";
+                document.getElementById("capnascidos").value="BRASILIENSES";
+                break;
+               }
+               case 'ES':{
+                document.getElementById("capital").value="VITÓRIA";
+                document.getElementById("nascidos").value="ESPÍRITO-SANTENSES";
+                document.getElementById("capnascidos").value="CAPIXABA";
+                break;
+               }
+               case 'GO':{
+                document.getElementById("capital").value="GOIÂNIA";
+                document.getElementById("nascidos").value="GOIANOS";
+                document.getElementById("capnascidos").value="GOIANIENSES";
+                break;
+               }
+               case 'MA':{
+                document.getElementById("capital").value="SÃO LUÍS";
+                document.getElementById("nascidos").value="MARANHENSES";
+                document.getElementById("capnascidos").value="LUDOVICENSES";
+                break;
+               }
+               case 'MG':{
+                document.getElementById("capital").value="BELO HORIZONTE";
+                document.getElementById("nascidos").value="MINEIRO";
+                document.getElementById("capnascidos").value="BELO-HORIZONTINOS";
+                break;
+               }
+               case 'MT':{
+                document.getElementById("capital").value="CUIABÁ";
+                document.getElementById("nascidos").value="MATO-GROSSENSES";
+                document.getElementById("capnascidos").value="CUIABANOS";
+                break;
+               }
+               case 'MS':{
+                document.getElementById("capital").value="CAMPO GRANDE";
+                document.getElementById("nascidos").value="MATO-GROSSENSES-DO-SUL";
+                document.getElementById("capnascidos").value="CAMPO-ALEGRENSES";
+                break;
+               }
+               case 'PA':{
+                document.getElementById("capital").value="BELÉM";
+                document.getElementById("nascidos").value="PARAENSES";
+                document.getElementById("capnascidos").value="BELENENSES";
+                break;
+               }
+               case 'PB':{
+                document.getElementById("capital").value="JOÃO PESSOA";
+                document.getElementById("nascidos").value="PARAIBANOS";
+                document.getElementById("capnascidos").value="PESSOENSES";
+                break;
+               }
+               case 'PE':{
+                document.getElementById("capital").value="RECIFE";
+                document.getElementById("nascidos").value="PERNAMBUCANOS";
+                document.getElementById("capnascidos").value="RECIFENSES";
+                break;
+               }
+               case 'PI':{
+                document.getElementById("capital").value="TERESINA";
+                document.getElementById("nascidos").value="PIAUIENSES";
+                document.getElementById("capnascidos").value="TERESINENSES";
+                break;
+               }
+               case 'PR':{
+                document.getElementById("capital").value="CURITIBA";
+                document.getElementById("nascidos").value="PARANAENSES";
+                document.getElementById("capnascidos").value="CURITIBANOS";
+                break;
+               }
+               case 'RJ':{
+                document.getElementById("capital").value="RIO DE JANEIRO";
+                document.getElementById("nascidos").value="FLUMINENSES";
+                document.getElementById("capnascidos").value="CARIOCAS";
+                break;
+               }
+               case 'RN':{
+                document.getElementById("capital").value="NATAL";
+                document.getElementById("nascidos").value="POTIGUARES";
+                document.getElementById("capnascidos").value="NATALENSES";
+                break;
+               }
+               case 'RO':{
+                document.getElementById("capital").value="PORTO VELHO";
+                document.getElementById("nascidos").value="RONDONIANOS";
+                document.getElementById("capnascidos").value="PORTO-VELHENSES";
+                break;
+               }
+               case 'RR':{
+                document.getElementById("capital").value="BOA VISTA";
+                document.getElementById("nascidos").value="RORAIMENSES";
+                document.getElementById("capnascidos").value="BOA-VISTENSES";
+                break;
+               }
+               case 'RS':{
+                document.getElementById("capital").value="PORTO ALEGRE";
+                document.getElementById("nascidos").value="GAÚCHOS";
+                document.getElementById("capnascidos").value="PORTO-ALEGRENSES";
+                break;
+               }
+                case 'SC':{
+                document.getElementById("capital").value="FLORIANÓPOLIS";
+                document.getElementById("nascidos").value="CATARINENSES";
+                document.getElementById("capnascidos").value="FLORIANOPOLITANOS";
+                break;
+               }
+               case 'SE':{
+                document.getElementById("capital").value="ARACAJÚ";
+                document.getElementById("nascidos").value="SERGIPANOS";
+                document.getElementById("capnascidos").value="ARACAJUANOS";
+                break;
+               }
+               case 'SP':{
+                document.getElementById("capital").value="SÃO PAULO";
+                document.getElementById("nascidos").value="PAULISTAS";
+                document.getElementById("capnascidos").value="PAULISTANOS";
+                break;
+               }
+               case 'TO':{
+                document.getElementById("capital").value="PALMAS";
+                document.getElementById("nascidos").value="TOCANTINENSES";
+                document.getElementById("capnascidos").value="PALMENSES";
+                break;
+               }
+    }
+}
